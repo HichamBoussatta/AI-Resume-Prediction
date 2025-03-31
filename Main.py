f7907cd28cab9ee8295a77e5ebf584f77f779e2b
Hicham BOUSSATTA <58424667+HichamBoussatta@users.noreply.github.com>
2025-03-31 17:06:40 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,26 @@
 import spacy
+import streamlit as st
+from spacy import displacy
+
+# Chargez le modèle spaCy en français
 nlp = spacy.load("fr_core_news_sm")
-print("Modèle chargé avec succès !")
+
+# Titre de l'application Streamlit
+st.title("Visualisation spaCy avec Streamlit")
+
+# Ajoutez un champ de texte où l'utilisateur peut saisir une phrase
+text = st.text_area("Entrez du texte :", "Jean Dupont travaille chez VISEO et il vit à Rabat.")
+
+# Traitez le texte avec spaCy
+doc = nlp(text)
+
+# Affichez les entités nommées
+if st.button('Afficher les entités nommées'):
+    st.subheader("Entités nommées :")
+    entities = [(ent.text, ent.label_) for ent in doc.ents]
+    st.write(entities)
+
+# Visualisation des dépendances avec displacy
+if st.button('Afficher la visualisation des dépendances'):
+    st.subheader("Visualisation des dépendances grammaticales :")
+    displacy.render(doc, style="dep", page=True)
