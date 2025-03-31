806a94e217b1d7c9c439ac6a1d423f8a3d3942c4
Hicham BOUSSATTA <58424667+HichamBoussatta@users.noreply.github.com>
2025-03-31 15:41:21 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,3 @@
 import spacy
-import os
-
-# Vérifier si le modèle est installé, sinon l'installer
-try:
-    nlp = spacy.load("fr_core_news_sm")
-except OSError:
-    os.system("python -m spacy download fr_core_news_sm")
-    nlp = spacy.load("fr_core_news_sm")
-
-print("Modèle spaCy chargé avec succès !")
+nlp = spacy.load("fr_core_news_sm")
+print("Modèle chargé avec succès !")
